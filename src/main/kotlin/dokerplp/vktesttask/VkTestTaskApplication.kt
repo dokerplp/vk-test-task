540ac9f9658591edeb25e390b1f2db467437af4a
Valerii Butorin <butorin.v.a@mail.ru>
2022-03-28 19:00:13 +0300

    init

---
+++ src/main/kotlin/dokerplp/vktesttask/VkTestTaskApplication.kt
@@ -0,0 +1,11 @@
+package dokerplp.vktesttask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VkTestTaskApplication
+
+fun main(args: Array<String>) {
+    runApplication<VkTestTaskApplication>(*args)
+}
---
+++ src/test/kotlin/dokerplp/vktesttask/VkTestTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package dokerplp.vktesttask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VkTestTaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
